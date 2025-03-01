6c5e8510d755dd4e0850e69c024dfe7c08ef4206
sofialazrak <sofia.lazrak@gmail.com>
2025-03-01 09:33:12 +0000

    Second commit

---
+++ app/api/auth/[...auth]/route.ts
@@ -0,0 +1,3 @@
+import { handlers } from "@/auth";
+
+export const { GET, POST } = handlers 
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,27 @@
+"use server"
+
+import { signInFormSchema } from "../validators"
+import { signIn, signOut } from "@/auth"
+import { isRedirectError } from "next/dist/client/components/redirect"
+
+// Sign in user with credentials
+export async function signInWithCredentials(prevState: unknown, formData: formData) {
+    try{
+        const user = signInFormSchema.parse({
+            email: formData.get('email'),
+            password: formData.get('password')
+        })
+        await signIn('credentials', user)
+        return { success: true, message: "Signed in successfully"}
+    } catch(error){
+        if(isRedirectError(error)){
+            throw error
+        }
+        return { success: false, message: "Invalid email or password" }
+    }
+}
+
+// Sign user out
+export async function signOutUser(){
+    await signOut()
+}
